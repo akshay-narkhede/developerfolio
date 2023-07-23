69b3b931d1a4ad4b2a0868041280eb6579c53a81
Akshay Narkhede <narkhede.aks@gmail.com>
2023-07-23 13:25:31 -0400

    added touch events in pong game mobile screen 3

--- public/game/gameScript.js
+++ public/game/gameScript.js
@@ -47,8 +47,8 @@ var Game = {
         this.canvas = document.querySelector('canvas');
         this.context = this.canvas.getContext('2d');
  
-        this.canvas.width = 1500;
-        this.canvas.height = 1000;
+        this.canvas.width = 1400;
+        this.canvas.height = 900;
  
         this.canvas.style.width = (this.canvas.width / 2) + 'px';
         this.canvas.style.height = (this.canvas.height / 2) + 'px';
